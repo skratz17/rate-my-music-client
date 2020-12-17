6b9ff2dc80aa4564395a987a666a7c8b0bbc3373
Jacob Eckert <jweckert17@gmail.com>
2020-12-17 10:03:39 -0600

    setup routing based on auth state, wrote tests for redirects based on auth state

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import { Home } from './home/Home';
 
 export const ApplicationViews = () => {
   return (
-    <div>Application Views</div>
+    <Switch>
+      <Route path="/home">
+        <Home />
+      </Route>
+
+      <Route path="/" >
+        <Redirect to="/home" />
+      </Route>
+    </Switch>
   );
 };
--- src/components/UnauthorizedUserViews.js
+++ src/components/UnauthorizedUserViews.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import { Login } from './auth/Login';
 import { Register } from './auth/Register';
 
 export const UnauthorizedUserViews = () => {
   return (
-    <>
-      <Route exact path="/" render={() => <Redirect to="/login" />} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </>
+    <Switch>
+      <Route path="/login">
+        <Login />
+      </Route>
+
+      <Route path="/register">
+        <Register />
+      </Route>
+
+      <Route path="/">
+        <Redirect to="/login" />
+      </Route>
+    </Switch>
   )
 };

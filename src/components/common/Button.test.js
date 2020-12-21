92a90fd54c21177e6b35716f1dd9e77af085ec8c
Jacob Eckert <jweckert17@gmail.com>
2020-12-20 22:33:05 -0600

    testing button and linkbutton

---
+++ src/components/common/Button.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { Button } from './Button';
+
+describe('button functionality', () => {
+  test('renders a button with given text that calls the given onClick function', async () => {
+    const mockClickHandler = jest.fn();
+    render(<Button onClick={mockClickHandler}>Test Button</Button>);
+
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
+    expect(button.textContent).toEqual('Test Button');
+
+    expect(mockClickHandler).toHaveBeenCalledTimes(0);
+    await userEvent.click(button);
+    expect(mockClickHandler).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/components/common/LinkButton.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+import { LinkButton } from './LinkButton';
+
+const renderComponent = ui => {
+  const history = createMemoryHistory();
+  history.push('/');
+
+  render(<Router history={history}>{ui}</Router>);
+
+  return history;
+};
+
+describe('LinkButton functionality', () => {
+  test('renders as a Link to the specific url', async () => {
+    const history = renderComponent(<LinkButton to="/test">Link Text</LinkButton>);
+
+    const link = screen.getByRole('link');
+    expect(link).toBeInTheDocument();
+    expect(link.textContent).toEqual('Link Text')
+
+    await userEvent.click(link);
+
+    expect(history.location.pathname).toEqual('/test');
+  });
+});

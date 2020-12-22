72b287a15988514577be1af227b5b441d5a5976f
Jacob Eckert <jweckert17@gmail.com>
2020-12-22 14:53:09 -0600

    add song and artist links in player controls and tests

--- src/components/player/PlayerControls.js
+++ src/components/player/PlayerControls.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { MdSkipPrevious, MdSkipNext, MdPlayArrow, MdPause } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 import { PlayerContext } from './PlayerProvider';
 import { PlayerControl } from './PlayerControl';
@@ -26,8 +27,12 @@ export const PlayerControls = () => {
   return (
     <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center text-center">
       <div className="mb-1">
-        <h3 className="text-xl">{currentSong.name}</h3>
-        <h4>{currentSong.artist.name}</h4>
+        <h3 className="text-xl">
+          <Link className="text-black hover:text-deepred" to={`/songs/${currentSong.id}`}>{currentSong.name}</Link>
+        </h3>
+        <h4>
+          <Link className="text-black hover:text-deepred" to={`/artists/${currentSong.artist.id}`}>{currentSong.artist.name}</Link>
+        </h4>
       </div>
 
       <div className="flex justify-evenly">
--- src/components/player/PlayerControls.test.js
+++ src/components/player/PlayerControls.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
 import { PlayerContext } from './PlayerProvider';
 import { PlayerControls } from './PlayerControls';
@@ -9,29 +11,53 @@ const mockPlay = jest.fn();
 const mockPause = jest.fn();
 const mockSkip = jest.fn();
 const mockCurrentSong = {
+  id: 1,
   name: 'Save a Secret for the Moon',
   artist: {
+    id: 2,
     name: 'The Magnetic Fields'
   }
 };
 
 const renderComponent = (ui, currentSong = mockCurrentSong, isPlaying = false) => {
+  const history = createMemoryHistory();
+
   render(
     <PlayerContext.Provider value={{
       play: mockPlay,
       pause: mockPause,
       skip: mockSkip,
       currentSong,
-      isPlaying}}>{ui}</PlayerContext.Provider>
+      isPlaying}}
+    >
+      <Router history={history}>
+        {ui}
+      </Router>
+    </PlayerContext.Provider>
   );
+
+  return history;
 };
 
 describe('player controls functionality', () => {
-  test('renders song and artist info if current song set', () => {
-    renderComponent(<PlayerControls />);
+  test('renders song name as link that routes to song page', async () => {
+    const history = renderComponent(<PlayerControls />);
 
-    expect(screen.getByText('Save a Secret for the Moon')).toBeInTheDocument();
-    expect(screen.getByText('The Magnetic Fields')).toBeInTheDocument();
+    const links = screen.getAllByRole('link');
+    expect(links[0]).toHaveTextContent('Save a Secret for the Moon');
+
+    await userEvent.click(links[0]);
+    expect(history.location.pathname).toEqual('/songs/1');
+  });
+
+  test('renders artist name as link that routes to artist page', async () => {
+    const history = renderComponent(<PlayerControls />);
+
+    const links = screen.getAllByRole('link');
+    expect(links[1]).toHaveTextContent('The Magnetic Fields');
+
+    await userEvent.click(links[1]);
+    expect(history.location.pathname).toEqual('/artists/2');
   });
 
   test('renders no songs queued message if no current song', () => {

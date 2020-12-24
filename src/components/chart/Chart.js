61772346b2e83b6afee04909c4eb0ee43b19a70e
Jacob Eckert <jweckert17@gmail.com>
2020-12-23 21:49:52 -0600

    better styling in chart and tests for chart and year select components

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -6,16 +6,20 @@ import { YearSelect } from './YearSelect';
 import { SongList } from '../song/SongList';
 import { PlayButton } from '../player/PlayButton';
 import { GenreAutocompleteSelector } from '../genre/GenreAutocompleteSelector';
+import { LoadingIndicator, WarningText } from '../common';
 
 export const Chart = () => {
   const [ chartParams, setChartParams ] = useState({ orderBy: 'avgRating', direction: 'desc' });
   const [ songs, isLoading, error ] = useApi(api.songs.list, chartParams);
 
   const handleChange = e => {
-    setChartParams(prevChartParams => ({
-      ...prevChartParams,
-      [e.target.name]: e.target.value
-    }));
+    const { name, value } = e.target;
+    setChartParams(prevChartParams => {
+      const updatedChartParams = { ...prevChartParams };
+      if(value === '') delete updatedChartParams[name];
+      else updatedChartParams[name] = value;
+      return updatedChartParams;
+    });
   };
 
   const handleGenreSelect = genres => {
@@ -30,18 +34,31 @@ export const Chart = () => {
 
   return (
     <div className="max-w-screen-lg mx-auto">
+      <h2 className="text-3xl text-center">The <span className="text-emerald">Charts</span></h2>
       <section>
-        <h2 className="text-3xl text-center">The <span className="text-emerald">Charts</span></h2>
-        <div>
-          Best songs from between <YearSelect name="startYear" value={chartParams.startYear || ''} onChange={handleChange} /> 
-          and <YearSelect name="endYear" value={chartParams.endYear || ''} onChange={handleChange} />
+        <LoadingIndicator isLoading={!songs && isLoading} />
+        <WarningText>{error}</WarningText>
+        <div className="text-xl my-2">
+          <label htmlFor="startYear">Best songs from between </label>
+          <YearSelect name="startYear" value={chartParams.startYear || ''} onChange={handleChange} /> 
+          <label htmlFor="endYear"> and </label>
+          <YearSelect name="endYear" value={chartParams.endYear || ''} onChange={handleChange} />
         </div>
-        <div>
-          In the genres: <GenreAutocompleteSelector className="p-2" name="genres" onSelect={handleGenreSelect} />
+        <div className="flex w-100 my-2">
+          <label className="mt-1 mr-2 text-xl" htmlFor="genres">In the genres:</label>
+          <div className="flex-grow">
+            <GenreAutocompleteSelector selectionsBelow className="p-2" name="genres" onSelect={handleGenreSelect} />
+          </div>
         </div>
       </section>
 
       <section>
+        { songs?.length > 0 && 
+          <div className="flex items-center">
+            <span className="text-3xl mr-2">Play All</span>
+            <PlayButton className="text-5xl" songs={songs} accessibleName="Play All Songs in Chart" />
+          </div>
+        }
         { songs && <SongList songs={songs} /> }
       </section>
     </div>
--- src/components/chart/YearSelect.js
+++ src/components/chart/YearSelect.js
@@ -8,8 +8,8 @@ export const YearSelect = props => {
   const { name, value, onChange } = props;
 
   return (
-    <select name={name} value={value} onChange={onChange}>
-      <option value="" disabled>any</option>
+    <select className="p-2 mx-2" id={name} name={name} value={value} onChange={onChange}>
+      <option value="">any</option>
       { YEARS.map(year => <option key={year} value={year}>{year}</option>).reverse() }
     </select>
   );
